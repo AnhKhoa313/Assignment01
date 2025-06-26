809575c08497c426c1f6d7c92b198c8af1554a12
AnhKhoa313 <khoaNASE182748@fpt.edu.vn>
2025-06-26 22:00:26 +0700

    Add files via upload

---
+++ NguyễnAnhKhoa_SE182748_A01/DataAccessLayer/CategoryDAO.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessObjects;
+using System.Xml.Linq;
+
+namespace DataAccessLayer
+{
+    public class CategoryDAO
+    {
+        private static List<Category> categories = new();
+        private static readonly object lockObj = new();
+
+        public static List<Category> GetCategories() => categories;
+
+        public static Category? GetCategoryById(int id) =>
+            categories.FirstOrDefault(c => c.CategoryID == id);
+
+        public static void AddCategory(Category category)
+        {
+            lock (lockObj)
+            {
+                categories.Add(category);
+            }
+        }
+
+        public static void UpdateCategory(Category category)
+        {
+            var existing = GetCategoryById(category.CategoryID);
+            if (existing != null)
+            {
+                existing.CategoryName = category.CategoryName;
+                existing.Description = category.Description;
+            }
+        }
+
+        public static void DeleteCategory(int id)
+        {
+            var cat = GetCategoryById(id);
+            if (cat != null)
+            {
+                categories.Remove(cat);
+            }
+        }
+    }
+}
