0b4382f41d0ff71e761eca7c82d975af93d5d351
AnhKhoa313 <khoaNASE182748@fpt.edu.vn>
2025-06-26 22:21:35 +0700

    DAL

---
+++ NguyễnAnhKhoa_SE182748_A01/DataAccessLayer/CustomerDAO.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using BusinessObjects;
+
+namespace DataAccessLayer
+{
+    public class CustomerDAO
+    {
+        private static List<Customer> customers = new List<Customer>();
+        private static readonly object lockObj = new();
+
+        public static List<Customer> GetCustomers() => customers;
+
+        public static Customer? GetCustomerById(int id) =>
+            customers.FirstOrDefault(c => c.CustomerID == id);
+
+        public static void AddCustomer(Customer customer)
+        {
+            lock (lockObj)
+            {
+                customers.Add(customer);
+            }
+        }
+
+        public static void UpdateCustomer(Customer customer)
+        {
+            var existing = GetCustomerById(customer.CustomerID);
+            if (existing != null)
+            {
+                existing.CompanyName = customer.CompanyName;
+                existing.ContactName = customer.ContactName;
+                existing.ContactTitle = customer.ContactTitle;
+                existing.Address = customer.Address;
+                existing.Phone = customer.Phone;
+            }
+        }
+
+        public static void DeleteCustomer(int id)
+        {
+            var customer = GetCustomerById(id);
+            if (customer != null)
+            {
+                customers.Remove(customer);
+            }
+        }
+    }
+}
---
+++ NguyễnAnhKhoa_SE182748_A01/DataAccessLayer/OrderDetailDAO.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessObjects;
+
+namespace DataAccessLayer
+{
+    public class OrderDetailDAO
+    {
+        private static List<OrderDetail> orderDetails = new();
+        private static readonly object lockObj = new();
+
+        public static List<OrderDetail> GetOrderDetails() => orderDetails;
+
+        public static List<OrderDetail> GetOrderDetailsByOrderId(int orderId) =>
+            orderDetails.Where(od => od.OrderID == orderId).ToList();
+
+        public static void AddOrderDetail(OrderDetail detail)
+        {
+            lock (lockObj)
+            {
+                orderDetails.Add(detail);
+            }
+        }
+
+        public static void DeleteOrderDetailsByOrderId(int orderId)
+        {
+            var items = orderDetails.Where(od => od.OrderID == orderId).ToList();
+            foreach (var item in items)
+            {
+                orderDetails.Remove(item);
+            }
+        }
+    }
+}
